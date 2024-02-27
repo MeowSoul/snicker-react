6aac9a89a56919ce4e726d6a4f0976116a701352
MeowSoul <agent_hammer@mail.ru>
2024-02-28 00:14:21 +0500

    Рефактор

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,38 +1,37 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react"
+import axios from "axios"
+import Card from "../components/Card"
 
-import Card from '../components/Card';
- 
 function Orders() {
-    const [orders, setOrders] = React.useState([]);
-    const [isLoading, setIsLoading] = React.useState(true);
+    const [orders, setOrders] = React.useState([])
+    const [isLoading, setIsLoading] = React.useState(true)
 
     React.useEffect(() => {
-        (async () => {
+        ;(async () => {
             try {
-                const { data } = await axios.get('https://0fa879b7c7c893b1.mokky.dev/orders');
-                setOrders(data.reduce((prev, obj) => [...prev, ...obj.items], []));
-                setIsLoading(false);
+                const { data } = await axios.get("https://0fa879b7c7c893b1.mokky.dev/orders")
+                setOrders(data.reduce((prev, obj) => [...prev, ...obj.items], []))
+                setIsLoading(false)
             } catch (error) {
-                alert('Ошибка при запросе заказов');
-                console.error(error);
+                alert("Ошибка при запросе заказов")
+                console.error(error)
             }
-        })();
-    }, []);
+        })()
+    }, [])
 
-    return(
+    return (
         <div className="content p-40">
             <div className="d-flex align-center justify-between mb-40">
                 <h1>Мои заказы</h1>
             </div>
 
             <div className="d-flex flex-wrap">
-            {(isLoading ? [...Array(8)] : orders).map((item, index) => (
-                <Card key={index} loading={isLoading} {...item} />
-            ))}
+                {(isLoading ? [...Array(8)] : orders).map((item, index) => (
+                    <Card key={index} loading={isLoading} {...item} />
+                ))}
             </div>
         </div>
-    );
+    )
 }
 
-export default Orders;
+export default Orders
